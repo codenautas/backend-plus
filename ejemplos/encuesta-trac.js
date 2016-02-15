eb34078dcb256eb17709c1e0bcb4a820090f7001
Emilio <Platzer>
2016-02-15 14:49:12 -0300

    Ya hace el login y configurable.

--- ejemplos/encuesta-trac.js
+++ ejemplos/encuesta-trac.js
@@ -2,8 +2,12 @@
 
 var backendPlus = require("..");
 var backend = new backendPlus.AppBackend();
+
 backend.init([
     'ejemplos/def-config.yaml',
     'ejemplos/local-config.yaml'
 ]);
 
+backend.app.get('/index', function(req,res){
+    res.end('here we are');
+});
--- ejemplos/install/create_db.sql
+++ ejemplos/install/create_db.sql
@@ -1,3 +1,15 @@
 create user beplus_example_user password 'beplus_example_3948812bdz';
 create database beplus_example_db owner beplus_example_user;
 \c beplus_example_db
+drop schema if exists bep cascade;
+create schema bep authorization beplus_example_user;
+grant all on schema bep to beplus_example_user;
+
+create table bep.users(
+  username text primary key,
+  md5pass text,
+  rol text
+);
+alter table bep."users" owner to beplus_example_user;
+
+insert into bep.users values ('tester', md5('1234tester'), 'test');
