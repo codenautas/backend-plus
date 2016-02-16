c6f2a9628f3dbeeb0694b4603c065abe615f408b
Emilio <Platzer>
2016-02-16 10:08:08 -0300

    es6

--- ejemplos/encuesta-trac.js
+++ ejemplos/encuesta-trac.js
@@ -4,19 +4,19 @@ const util = require('util');
 
 var backendPlus = require("..");
 
-function AppTrac(){
-    AppTrac.super_.call(this);
+class AppTrac extends backendPlus.AppBackend{
+    /*
+    constructor(){
+        super();
+    }
+    */
+    configList(){
+        return super.configList().concat([
+            'ejemplos/def-config.yaml',
+            'ejemplos/local-config.yaml'
+        ]);
+    }
+    get rootPath(){ return __dirname +'/'; }
 }
-
-util.inherits(AppTrac, backendPlus.AppBackend);
-
-AppTrac.prototype.configList=function configList(){
-    return AppTrac.super_.prototype.configList.call(this).concat([
-        'ejemplos/def-config.yaml',
-        'ejemplos/local-config.yaml'
-    ]);
-};
-
-AppTrac.prototype.rootPath = __dirname +'/';
 
 new AppTrac().start();
