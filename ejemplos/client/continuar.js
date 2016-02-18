c55d2ba4fefd0664da286b61345d464fade2c83b
estefi-capece <eycapece@gmail.com>
2016-02-18 10:19:19 -0300

    preguntas y respuestas como listas ordenadas con ids

--- ejemplos/client/continuar.js
+++ ejemplos/client/continuar.js
@@ -13,34 +13,28 @@ window.addEventListener("load",function(){
         var preguntasYrespuestas;
         var cuestionario=[];
         var preguntas=[];
-       // var respuestas=[];
         test=test.estructura.forEach(function(fila){
-            //console.log(fila)
             if(fila.tipo=='TITULO'){
                 titulo=html.h1(JSON.stringify(fila.texto));
-                
             }
             if(fila.tipo=='PREGUNTA'){
-                
-                preguntas.push(html.div(JSON.stringify(fila.texto)));
-                
+                preguntas.push(html.tr({id:fila.id},JSON.stringify(fila.texto)));
+                preguntas.push(html.ol({class:"respuestas"}));
                 var opciones=fila.opciones;
                 var respuestas=[];
                 opciones.forEach(function(opcion){
-                    preguntas.push(html.div(JSON.stringify(opcion.texto)));             
+                    preguntas.push(html.li({class:"opciones"},JSON.stringify(opcion.texto)));  
                 });
-
+                preguntas.push(html.br());
             }
             console.log(preguntas);
-
-            
         });
 
         pantalla.appendChild(html.div([
             html.pre(result),
             html.hr(),
             html.tr([titulo]),
-            html.tr(preguntas),
+            html.div(preguntas),
             html.pre(JSON.stringify(JSON.parse(result),null,'  ')),
         ]).create());
         
