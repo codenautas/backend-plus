9e946d300f2db38be1e645a67d9c01c464dba041
Emilio <Platzer>
2016-02-19 17:40:34 -0300

    reordenamiento de código

--- ejemplos/client/continuar.js
+++ ejemplos/client/continuar.js
@@ -2,45 +2,46 @@
 
 var html=jsToHtml.html;
 
+function presentarFormulario(estructura){
+    var titulo=[];
+    var preguntas=[];
+    estructura.forEach(function(fila){
+        if(fila.tipo=='TITULO'){
+            titulo.push(html.div({id:"titulo"},JSON.stringify(fila.texto)))
+        }
+        if(fila.tipo=='PREGUNTA'){
+            preguntas.push(html.tr({"class":"preguntas",id:fila.id},fila.texto));
+            var respuestas=[];
+            preguntas.push(html.br());
+            fila.opciones.forEach(function(opcion){
+                preguntas.push(html.td([         
+                    html.label({"for":"opciones"},opcion.texto),
+                    html.input({"class":"opc-resp",type:"checkbox"})
+                ]));
+                preguntas.push(html.br());
+            });
+            preguntas.push(html.br());
+        }
+        console.log(preguntas);
+    });
+    pantalla.appendChild(html.div([
+        html.tr(titulo),
+        html.div({"class":"bloque"},preguntas),
+    ]).create());
+}
+
+function ponerDatos(datos){
+    
+}
+
 window.addEventListener("load",function(){
    // console.log("HOLA MUNDO");
     AjaxBestPromise.get({
         url:'/info-enc-act',
         data:{}
-    }).then(function(result){
-        var test=JSON.parse(result);
-        var titulo=[];
-        var preguntasYrespuestas;
-        var cuestionario=[];
-        var preguntas=[];
-        test=test.estructura.forEach(function(fila){
-            if(fila.tipo=='TITULO'){
-                titulo.push(html.div({id:"titulo"},JSON.stringify(fila.texto)))
-//                titulo=html.h1({id:"titulo"},JSON.stringify(fila.texto));
-            }
-            if(fila.tipo=='PREGUNTA'){
-                preguntas.push(html.tr({"class":"preguntas",id:fila.id},JSON.stringify(fila.texto)));
-                var respuestas=[];
-                preguntas.push(html.br());
-                fila.opciones.forEach(function(opcion){
-                    preguntas.push(html.td([         
-                        html.label({"for":"opciones"},JSON.stringify(opcion.texto)),
-                        html.input({"class":"opc-resp",type:"checkbox"})
-                    ]));
-                    preguntas.push(html.br());
-                });
-                preguntas.push(html.br());
-            }
-            console.log(preguntas);
-        });
-
-        pantalla.appendChild(html.div([
-            html.tr(titulo),
-            html.div({"class":"bloque"},preguntas),
-            html.hr(),
-            html.pre(JSON.stringify(result)),
-            html.pre(JSON.stringify(JSON.parse(result),null,'  ')),
-        ]).create());
-        
+    }).then(function(resultJson){
+        var result=JSON.parse(resultJson);
+        presentarFormulario(result.estructura);
+        ponerDatos(result.datos);
     });
 });
