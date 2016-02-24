cab55e35fb3a77a6c5b93f8f55ed31d569ed9898
estefi-capece <eycapece@gmail.com>
2016-02-24 11:36:42 -0300

    No inserta datos en tabla

--- ejemplos/encuesta-trac.js
+++ ejemplos/encuesta-trac.js
@@ -2,46 +2,52 @@
 
 const util = require('util');
 
+var Promises = require('best-promise');
+
 var backendPlus = require("..");
-var provisorio =
-{
-	id:{enc: 9921, "for": "TRAC"},
-	estructura:[
-		{ tipo:'TITULO', texto:"EVALUACION DE SOPORTE INFORMATICO"},
-		{ tipo:'PREGUNTA', id: "1", variable: "v1", texto: "Tiempo transcurrido entre el pedido y la asistencia", opciones:[
-			{opcion: "a", texto:"24hs"},
-			{opcion: "b", texto:"48hs"},
-			{opcion: "c", texto:"72hs"},
-			{opcion: "d", texto:"Más de 72 hs"}
-		]},
-		{ tipo:'PREGUNTA', id: "2", variable: "v2", texto: "Tiempo que llevo la asistencia", opciones:[
-			{opcion: "a", texto:"24hs"},
-			{opcion: "b", texto:"48hs"},
-			{opcion:"c",  texto:"72hs"},
-			{opcion:"d",  texto:"Más de 72hs"}
-		]},
-		{tipo:'PREGUNTA', id:"3", variable:"v3",texto: "Cumplimiento con la asistencia",opciones:[
-			{opcion: "a", texto:"Total"},
-			{opcion:"b",  texto:"Parcial"},
-			{opcion:"c",  texto:"No cumplió (aclarar en observaciones)"},
-			{opcion:"d",  texto:"No aplica"}
-		]},
-		{tipo:"PREGUNTA", id:"4", variable:"v4",texto: "Calidad de la asistencia",opciones:[
-			{opcion:"a",  texto:"Muy buena"},
-			{opcion:"b",  texto:"Buena"},
-			{opcion:"c",  texto:"Regular (aclarar en observaciones)"},
-			{opcion:"d",  texto:"Mala (aclarar en observaciones)"},
-			{opcion:"e",  texto:"No aplica"}
-		]}
-	],
-	datos:{v1: "a", v2: "b", v3: null, v4: null, v5: null},
-	estado:'con-datos',
+var provisorio = {
+    id:{enc: Math.random(), "for": "TRAC"},
+    estructura:[
+        { tipo:'TITULO', texto:"EVALUACION DE SOPORTE INFORMATICO"},
+        { tipo:'PREGUNTA', id: "1", variable: "v1", texto: "Tiempo transcurrido entre el pedido y la asistencia", opciones:[
+            {opcion: "a", texto:"24hs"},
+            {opcion: "b", texto:"48hs"},
+            {opcion: "c", texto:"72hs"},
+            {opcion: "d", texto:"Más de 72 hs"}
+        ]},
+        { tipo:'PREGUNTA', id: "2", variable: "v2", texto: "Tiempo que llevo la asistencia", opciones:[
+            {opcion: "a", texto:"24hs"},
+            {opcion: "b", texto:"48hs"},
+            {opcion:"c",  texto:"72hs"},
+            {opcion:"d",  texto:"Más de 72hs"}
+        ]},
+        {tipo:'PREGUNTA', id:"3", variable:"v3",texto: "Cumplimiento con la asistencia",opciones:[
+            {opcion: "a", texto:"Total"},
+            {opcion:"b",  texto:"Parcial"},
+            {opcion:"c",  texto:"No cumplió (aclarar en observaciones)"},
+            {opcion:"d",  texto:"No aplica"}
+        ]},
+        {tipo:"PREGUNTA", id:"4", variable:"v4",texto: "Calidad de la asistencia",opciones:[
+            {opcion:"a",  texto:"Muy buena"},
+            {opcion:"b",  texto:"Buena"},
+            {opcion:"c",  texto:"Regular (aclarar en observaciones)"},
+            {opcion:"d",  texto:"Mala (aclarar en observaciones)"},
+            {opcion:"e",  texto:"No aplica"}
+        ]}
+    ],
+    datos:{v1: "a", v2: "b", v3: null, v4: null, v5: null},
+    estado:'con-datos',
 };
 
+var registroVacio={};
+provisorio.estructura.forEach(function(celda){
+    if(celda.tipo=='PREGUNTA'){
+        registroVacio[celda.variable]=null;
+    }
+});
+
+
 class AppTrac extends backendPlus.AppBackend{
-    constructor(){
-        super();
-	}
     configList(){
         return super.configList().concat([
             'ejemplos/def-config.yaml',
@@ -53,26 +59,47 @@ class AppTrac extends backendPlus.AppBackend{
         this.app.get('/info-enc-act', function(req, res){
             res.end(JSON.stringify(provisorio));
         });
-		var be = this;
+        var be = this;
         this.app.post('/guardar', function(req, res){
-			var prov=provisorio;
-			return be.getDbClient().then(function(client) {
-				return client.query("select username, idencuesta, variable from bep.datos").fetchOneRowIfExists();
-			}).then(function(data) {
-				var sql = '';
-				if(data.rowCount > 0) {
-					sql = "UPDATE bep.datos SET variable = $4 WHERE username = $1 AND idencuesta = $2 AND variable = $3";
-				} else {
-					sql = "INSERT INTO bep.datos (username, idencuesta, variable, valor) VALUES($1, $2, $3, $4)";
-				}
-				return client.query(sql,[prov.id.enc, 'tester', info.variable, info.valor]);
-			}).then(function() {
-				var info=JSON.parse(req.body.info);
-				console.log("data", data);
-				res.end("a las " + data.row.timetz + ", recibi: "+JSON.stringify(info));
-			}).catch(function(err) {
-				res.end("error: "+err);
-			});
+            var parametros=JSON.parse(req.body.info);
+            console.log('entra a /guardar',parametros)
+            var client;
+            return be.getDbClient().then(function(cli) {
+                client=cli;
+                return client.query("BEGIN TRANSACTION").execute();
+            }).then(function() {
+                return client.query("SELECT id, contenido, estado FROM bep.datos WHERE id = $1 FOR UPDATE",[parametros.id]).fetchOneRowIfExists();
+            }).then(function(data) {
+                console.log('veo filas',data.rowCount,parametros)
+                return Promises.sleep(1000).then(function(){
+                    console.log('terminé de esperar',parametros)
+                    return data;
+                });
+            }).then(function(data) {
+                var sql = '';
+                if(data.rowCount == 0) {
+                    console.log('tengo que hacer el insert', parametros)
+                    sql = "INSERT INTO bep.datos (id, contenido) SELECT $1, $2 FROM bep.datos WHERE NOT EXISTS (SELECT 1 FROM bep.datos WHERE id=$3)";
+                    return client.query(sql,[parametros.id, registroVacio, parametros.id]).execute();
+                }
+            }).then(function() {
+                console.log('termine el insert', parametros)
+                var sql = "UPDATE bep.datos SET contenido = contenido || $2, estado='pendiente' WHERE id = $1 RETURNING contenido";
+                // var nuevoValorCampo
+                return client.query(sql,[parametros.id, {[parametros.variable]: parametros.valor}]).execute();
+            }).then(function(datos) {
+                return client.query("COMMIT").execute().then(function(){ return datos; });
+            }).then(function(datos) {
+                console.log("data", datos);
+                res.end("recibi: "+JSON.stringify(datos.rows));
+            }).catch(function(err) {
+                console.log("error: "+err);
+                res.end("error: "+err);
+            }).then(function(){
+                client.done();
+            }).catch(function(err) {
+                console.log("error al cerrar: "+err);
+            });
         });
     }
     get rootPath(){ return __dirname +'/'; }
--- ejemplos/install/encuesta.sql
+++ ejemplos/install/encuesta.sql
@@ -1,18 +1,32 @@
--- DROP TABLE bep.datos;
+﻿-- DROP TABLE bep.datos;
 
-CREATE TABLE bep.datos
-(
-  username text,
-  idencuesta character varying NOT NULL,
-  variable character varying,
-  valor character varying,
-  CONSTRAINT encuesta_pk PRIMARY KEY (idencuesta),
-  CONSTRAINT username_fk FOREIGN KEY (username)
-      REFERENCES bep.users (username) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
+CREATE TYPE bep.estados as enum ('vacio','pendiente','ingresado');
+
+CREATE TABLE bep.datos(
+  id jsonb primary key,
+  contenido jsonb,
+  estado bep.estados default 'vacio',
+  modif timestamp default current_timestamp,
+  modiu text default user
 );
-ALTER TABLE bep.datos
-  OWNER TO postgres;
+ALTER TABLE bep.datos 
+  OWNER TO beplus_example_user;
+
+/*
+insert into bep.datos (id, contenido) 
+  values ('{"id":4}', '{"v1": "a"}');
+
+select * from bep.datos;
+
+alter type bep.estados add value 'cerrado';
+
+insert into bep.datos (id, contenido) values ('{"id":5}', '{"v1": "b"}');
+insert into bep.datos (id, contenido) values ('{"id":6, "for":"TRAC"}', '{"v1": "b", "v2":"a"}');
+
+update bep.datos set estado='cerrado' where id='{"id":5}';
+update bep.datos set estado='pendiente' where id='{"id":6, "for":"TRAC"}';
+
+select * from bep.datos order by id;
+select * from bep.datos order by estado;
+
+*/
