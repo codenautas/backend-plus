cc4903317cb71bbca3b5691e7f14378838f8b8fb
estefi-capece <eycapece@gmail.com>
2016-03-01 14:54:45 -0300

    el texto de la pantalla fin-ingreso lo toma a partir de estructura-trac.yaml

--- ejemplos/client/fin-ingreso.js
+++ ejemplos/client/fin-ingreso.js
@@ -1,13 +1,20 @@
 var html=jsToHtml.html;
 
-function presentarPlacaFin(){
+function presentarPlacaFin(textoFinIngreso){
     var placa=[];
-    var mensaje='Gracias por completar la encuesta'
+    var mensaje=textoFinIngreso
     placa.push(html.div({id:"fin-ingreso"},mensaje));
     pantallaIngreso.appendChild(html.div(placa).create());
 }
 
 window.addEventListener("load",function(){
-    presentarPlacaFin();
-    //pantallaIngreso.appendChild(html.div('Gracias por completar la encuesta').create());
+   
+   AjaxBestPromise.post({
+        url:'/info-enc-act',
+        data:{info:"{}"}
+    }).then(function(resultJson){
+        var result=JSON.parse(resultJson);
+        var textoFinIngreso=result.estructura.textos.placas['fin-ingreso'];
+        presentarPlacaFin(textoFinIngreso);
+    });
 });
