106ec95ae264703361799ac3303ca4511dd613c0
estefi-capece <eycapece@gmail.com>
2016-03-01 14:20:25 -0300

    arma la pantalla usando js-to-html y le di color al .styl

--- ejemplos/client/fin-ingreso.js
+++ ejemplos/client/fin-ingreso.js
@@ -1,19 +1,13 @@
 var html=jsToHtml.html;
 
-
+function presentarPlacaFin(){
+    var placa=[];
+    var mensaje='Gracias por completar la encuesta'
+    placa.push(html.div({id:"fin-ingreso"},mensaje));
+    pantallaIngreso.appendChild(html.div(placa).create());
+}
 
 window.addEventListener("load",function(){
-    //document.getElementById('status').innerHTML = "Listo.";
-   // console.log("HOLA MUNDO");
-   // AjaxBestPromise.post({
-   //     url:'/fin-ingreso',
-   //     data:{info:"{}"}
-   // }).then(function(resultJson){
-   //     var result=JSON.parse(resultJson);
-        // presentarFormulario(result.estructura.formularios[result.id["for"]]).then(function(divFormulario){
-            // divFormulario.idRegistro = result.id;
-            // ponerDatos(result.datos);
-        // });
-    // });
-    pantallaIngreso.appendChild(html.div('Gracias por completar la encuesta').create());
+    presentarPlacaFin();
+    //pantallaIngreso.appendChild(html.div('Gracias por completar la encuesta').create());
 });
