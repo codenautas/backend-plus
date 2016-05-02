19d976baf28f52472b47ee0f502f3bddd4fa6c95
estefi-capece <eycapece@gmail.com>
2016-05-02 16:08:00 -0300

    texto automático

--- ejemplos/client/continuar.js
+++ ejemplos/client/continuar.js
@@ -12,7 +12,7 @@ function presentarFormulario(estructura){
         if(fila.tipo=='titulo'){
             contenidoCelda.push(html.div({"class":"titulo", id:"titulo"},fila.titulo))
         }
-        if(fila.tipo=='otro'){
+        if(fila.tipo=='texto'){
             contenidoCelda.push(html.div({"class":"texto"},fila.texto))
         }
         if(fila.tipo=='pregunta'){
