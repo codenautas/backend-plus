58075cbc7b2bbd024feb7ca602be8b4c26d01864
estefi-capece <eycapece@gmail.com>
2016-06-10 17:06:10 -0300

    Agregamos los datos.

--- examples/tables/client/index.js
+++ examples/tables/client/index.js
@@ -17,22 +17,33 @@ function prepareTableButtons(){
                 }
             }).then(JSON.parse).then(function(tableDef){
                 console.log(tableDef);
-                var table = html.table([html.thead([html.tr(
-                    tableDef.fields.map(function(fieldDef){
-                        return html.th(fieldDef.title);
-                    })
-                )])]).create();
+                var tableElement = html.table([
+                    html.caption(tableDef.title),
+                    html.thead([html.tr(
+                        tableDef.fields.map(function(fieldDef){
+                            return html.th(fieldDef.title);
+                        })
+                    )]),
+                    html.tbody()
+                ]).create();
                 layout.innerHTML='';
-                layout.appendChild(table);
+                layout.appendChild(tableElement);
+                return {element: tableElement, def: tableDef};
             });
             AjaxBestPromise.post({
                 url:'table/data',
                 data:{
                     table:tableName
                 }
-            }).then(function(result){
-                structureRequest.then(function(){
-                    layout.appendChild(html.pre('data '+result+' ----\n ').create());
+            }).then(JSON.parse).then(function(rows){
+                structureRequest.then(function(table){
+                    var tbody = table.element.tBodies[0];
+                    rows.forEach(function(row){
+                        var tr = tbody.insertRow(-1);
+                        table.def.fields.forEach(function(fieldDef){
+                            tr.appendChild(html.td([row[fieldDef.name]]).create());
+                        });
+                    });
                 });
             });
         });
