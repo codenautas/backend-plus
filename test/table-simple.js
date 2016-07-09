edbdfc223f00b974f67068a5600c71e8a2bfd35f
Emilio <emilioplatzer@gmail.com>
2016-07-09 18:39:57 -0300

    restauro los tests.

--- test/table-simple.js
+++ test/table-simple.js
@@ -1,17 +1,18 @@
 "use strict";
 
-var tableDefCompleter = require('../lib/table-def-completer.js');
-
-module.exports = tableDefCompleter({
-    name:'employees',
-    title:'employees of this company',
-    fields:[
-        {name:'id_type'            , typeName:'text'   , nullable:false},
-        {name:'id'                 , typeName:'integer', nullable:false},
-        {name:'first_name'         , typeName:'integer', nullable:false},
-        {name:'last_name'          , typeName:'integer', nullable:false},
-        {name:'birth_date'         , typeName:'date'                   },
-        {name:'salary'             , typeName:'numeric'                },
-    ],
-    primaryKey:['id_type','id'],
-});
+module.exports = function(context){
+    return context.be.tableDefCompleter({
+        name:'employees',
+        title:'employees of this company',
+        editable:true,
+        fields:[
+            {name:'id_type'            , typeName:'text'   , nullable:false},
+            {name:'id'                 , typeName:'integer', nullable:false},
+            {name:'first_name'         , typeName:'integer', nullable:false},
+            {name:'last_name'          , typeName:'integer', nullable:false},
+            {name:'birth_date'         , typeName:'date'                   },
+            {name:'salary'             , typeName:'numeric'                },
+        ],
+        primaryKey:['id_type','id'],
+    });
+};
