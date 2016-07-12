16515973dfd4f24139a09280100464261fe3e314
Diego F.(EW7) <diegoefe@gmail.com>
2016-07-12 18:45:01 -0300

    Genero html con specs para testear (a verificar)

--- test/checklist/generate.js
+++ test/checklist/generate.js
@@ -30,19 +30,47 @@ var checks = [
     {action:'Server: on, Network: on', message:'none'},
 ];
 
+var firstRowCells = [];
+firstRowCells.push(html.th(" "));
+firstRowCells.push(html.th({colspan:browsers.length}, 'Browsers'));
+
+var secondRowCells = [];
+secondRowCells.push(html.th("Check"));
+
+var clCells = [];
+
+browsers.forEach(function(browser) {
+   secondRowCells.push(html.th(browser));
+   clCells.push(html.td(" "));
+});
+
+var rows = [];
+rows.push(html.tr(firstRowCells));
+rows.push(html.tr(secondRowCells));
+
+checks.forEach(function(check) {
+   oses.forEach(function(os) {
+      machines.forEach(function(mac) {
+        var desc = [];
+        desc.push('Action: '+check.action);
+        desc.push(' Machine: '+mac+' ('+os+')');
+        desc.push(' Expecting: '+check.message);
+        var row = [];
+        row.push(html.td(desc.join(",")));
+        rows.push(html.tr(row.concat(clCells)));
+      });
+   });
+});
+
 var pageName = "Checklist for backend-plus";
 var data = html.html([
     html.head([
-        html.title(pageName)
+        html.title(pageName),
+        html.link({type:'text/css', rel:'stylesheet', href:'checklist.css'})
     ]),
     html.body([
         html.h2(pageName),
-        html.table({id:'t1'}, [
-            html.tr([
-                html.td("td1"),
-                html.td("td2"),
-            ])
-        ])
+        html.table({id:'t1'}, rows)
     ])
 ]);
 fs.writeFile(path.resolve(__dirname+"/checklist.html"), data.toHtmlText({pretty:true})).then();
