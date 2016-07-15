abe047b6ba04aed6cb9221569f7d6723f098b86a
Emilio <Platzer>
2016-07-15 15:54:17 -0300

    sigue habiendo un problema al levantar dates

--- test/fixtures/fixture-select.js
+++ test/fixtures/fixture-select.js
@@ -20,9 +20,9 @@
     action: 'table/save-record',
     parameters: {
         table: 'employees',
-        primaryKeyValues: ['card',123456],
+        primaryKeyValues: ['passport',123456],
         newRow: {birth_date: new Date(1990,1-1,8)},
         status: 'retrieved'
     },
-    expected: {id_type: 'card',id: 123456,first_name: 'Bob' ,last_name: 'Smith',birth_date: new Date(1990,1-1,8),salary: null },
+    expected: {id_type: 'passport',id: 123456,first_name: 'Bob' ,last_name: 'Smith',birth_date: new Date(1990,1-1,8),salary: null },
 }]
