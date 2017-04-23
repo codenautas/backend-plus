c90e8bf294e59b8a0f2bc94f7b3c489763c9eee5
Emilio <emilioplatzer@gmail.com>
2017-04-23 20:24:54 -0300

    bug en botón azul de la escalerita

--- for-client/my-things.js
+++ for-client/my-things.js
@@ -547,8 +547,7 @@ myOwn.getUniqueDomId = function getUniqueDomId(){
 }
 
 myOwn.prepareFloating3dots = function prepareFloating3dots(){
-    return ;
-    if(!my.imgFloating3dots){
+    if(!my.imgFloating3dots && false){
         var img = html.img({src:my.path.img+'floating-3dots.png', class:'floating-img'}).create();
         my.imgFloating3dots=img;
         var rePosition = function(){
