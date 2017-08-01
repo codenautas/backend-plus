100882214266a060e8a07e8b43057ab83d3543b0
Manuel De la Penna <manueldelapenna@gmail.com>
2017-08-01 09:27:31 -0300

    #97 se da la posibilidad de importar en un campo especial las columnas sobrantes

--- examples/tables/server/table-isotopes.js
+++ examples/tables/server/table-isotopes.js
@@ -10,10 +10,11 @@ module.exports = function(context){
             update:context.user.rol==='boss',
         },
         fields:[
-            {name:'atomic_number', title:'A#', typeName:'integer', width:100, nullable:false, orderForInsertOnly:'1'      },
-            {name:'mass_number'         , typeName:'integer', width:100,                      orderForInsertOnly:'2'      },
-            {name:'order'               , typeName:'integer', width:100,                      orderForInsertOnly:'4'      },
-            {name:'stable'              , typeName:'boolean', width:100,                                                  },
+            {name:'atomic_number', title:'A#', typeName:'integer' , width:100, nullable:false,      orderForInsertOnly:'1' },
+            {name:'mass_number'              , typeName:'integer' , width:100,                      orderForInsertOnly:'2' },
+            {name:'order'                    , typeName:'integer' , width:100,                      orderForInsertOnly:'4' },
+            {name:'stable'                   , typeName:'boolean' , width:100,                                             },
+            {name:'others'                   , typeName:'text'                , defaultForOtherFields: true                },
         ],
         filterColumns:[
             {column:'atomic_number', operator:'>', value:context.be.internalData.filterAtomicNumberForIsotopes}
