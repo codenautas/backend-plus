d3bc7ee6894f18cc2977a4930db111d619f9907c
Manuel De la Penna <manueldelapenna@gmail.com>
2017-09-01 14:53:36 -0300

    #49 implementación de bitácora de procedimientos

--- examples/tables/server/procedures-examples.js
+++ examples/tables/server/procedures-examples.js
@@ -36,6 +36,38 @@ ProceduresExamples = [
             });
         }
     },
+    {
+        action:'bitacora/prueba',
+        parameters:[
+            {name:'first_atomic_number', defaultValue:10, typeName:'integer', references:'ptable'},
+            {name:'last_atomic_number' , defaultValue:99, typeName:'integer', references:'ptable'},
+            {name:'group'         , defaultValue:'Actinide', typeName:'text', references:'pgroups'},
+        ],
+        bitacora:{error:true, always:true},
+        coreFunction:function(context, parameters){
+            return context.client.query(
+                `SELECT count(*) as number_of_elements 
+                   FROM ptable p left join isotopes i on p.atomic_number=i.atomic_number 
+                   WHERE i.atomic_number IS NULL
+                     AND p.atomic_number between coalesce($1,0) and coalesce($2,999)`,
+                [parameters.first_atomic_number, parameters.last_atomic_number]
+            ).fetchUniqueRow().then(function(result){
+                return result.row.number_of_elements + " - Se registró ejecución en bitácora.";
+            });
+        }
+    },
+    {
+        action:'bitacora/prueba-error',
+        parameters:[
+            {name:'first_atomic_number', defaultValue:10, typeName:'integer', references:'ptable'},
+            {name:'last_atomic_number' , defaultValue:99, typeName:'integer', references:'ptable'},
+            {name:'group'         , defaultValue:'Actinide', typeName:'text', references:'pgroups'},
+        ],
+        bitacora:{error:true, always:true},
+        coreFunction:function(context, parameters){
+            throw Error("Error al ejecutar el procedimiento. Se registró ejecución en la bitácora.");
+        }
+    },
 ];
 
 module.exports = ProceduresExamples;
