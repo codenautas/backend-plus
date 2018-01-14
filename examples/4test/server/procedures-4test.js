89440dfb330357ebe9ee8334a9692cb7d9969037
Emilio <emilioplatzer@gmail.com>
2018-01-14 18:04:48 -0300

    Agrego una prueba con puppeteer

---
+++ examples/4test/server/procedures-4test.js
@@ -0,0 +1,91 @@
+"use strict";
+var json4all = require('json4all');
+var ProceduresExamples = {};
+
+ProceduresExamples = [
+    {
+        action:'getpictureurl',
+        parameters:[
+            {name:'atomic_number'},
+        ],
+        coreFunction:function(context, parameters){
+            return context.client.query(
+                "SELECT url FROM element_images WHERE atomic_number=$1 ORDER BY kind",
+                [parameters.atomic_number]
+            ).fetchAll().then(function(result){
+                return result.rows;
+            });
+        }
+    },
+    {
+        action:'count/without-isotopes',
+        parameters:[
+            {name:'first_atomic_number', defaultValue:10, typeName:'integer', references:'ptable'},
+            {name:'last_atomic_number' , defaultValue:99, typeName:'integer', references:'ptable'},
+            {name:'group'         , defaultValue:'Actinide', typeName:'text', references:'pgroups'},
+        ],
+        coreFunction:function(context, parameters){
+            return context.client.query(
+                `SELECT count(*) as number_of_elements 
+                   FROM ptable p left join isotopes i on p.atomic_number=i.atomic_number 
+                   WHERE i.atomic_number IS NULL
+                     AND p.atomic_number between coalesce($1,0) and coalesce($2,999)`,
+                [parameters.first_atomic_number, parameters.last_atomic_number]
+            ).fetchUniqueRow().then(function(result){
+                return result.row.number_of_elements;
+            });
+        }
+    },
+    {
+        action:'example/date',
+        parameters:[
+            {name:'date', typeName:'date'},
+        ],
+        resultClass:'result-pre',
+        coreFunction:function(context, parameters){
+            var answer=[]
+            answer.push(parameters.date);
+            answer.push(parameters.date.isRealDate);
+            answer.push(json4all.stringify(parameters.date));
+            answer.push(json4all.parse(json4all.stringify(parameters.date)));
+            answer.push(json4all.parse(json4all.stringify(parameters.date)).isRealDate);
+            console.log('xxxxxxxxx va');
+            console.log(answer);
+            return answer;
+        }
+    },
+    {
+        action:'bitacora/prueba',
+        parameters:[
+            {name:'first_atomic_number', defaultValue:10, typeName:'integer', references:'ptable'},
+            {name:'last_atomic_number' , defaultValue:99, typeName:'integer', references:'ptable'},
+            {name:'group'         , defaultValue:'Actinide', typeName:'text', references:'pgroups'},
+        ],
+        bitacora:{error:true, always:true},
+        coreFunction:function(context, parameters){
+            return context.client.query(
+                `SELECT count(*) as number_of_elements 
+                   FROM ptable p left join isotopes i on p.atomic_number=i.atomic_number 
+                   WHERE i.atomic_number IS NULL
+                     AND p.atomic_number between coalesce($1,0) and coalesce($2,999)`,
+                [parameters.first_atomic_number, parameters.last_atomic_number]
+            ).fetchUniqueRow().then(function(result){
+                return result.row.number_of_elements + " - Se registró ejecución en bitácora.";
+            });
+        }
+    },
+    {
+        action:'bitacora/prueba-error',
+        parameters:[
+            {name:'first_atomic_number', defaultValue:10, typeName:'integer', references:'ptable'},
+            {name:'last_atomic_number' , defaultValue:99, typeName:'integer', references:'ptable'},
+            {name:'group'         , defaultValue:'Actinide', typeName:'text', references:'pgroups'},
+        ],
+        bitacora:{error:true, always:true},
+        coreFunction:function(context, parameters){
+            throw Error("Error al ejecutar el procedimiento. Se registró ejecución en la bitácora.");
+        }
+    },
+];
+
+module.exports = ProceduresExamples;
---
+++ test/puppeteer/first-step.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    page.on('console', msg => console.log('PAGE LOG:', ...msg.args));
+    await page.setViewport({width:1360, height:768});
+    await page.goto('http://localhost:3333');
+    var rect = await page.evaluate(() => {
+        document.getElementById('username').value='bob'; 
+        document.getElementById('password').value='bobpass'; 
+    });
+    await page.click('[type=submit]');
+    await page.waitForSelector('#light-network-signal')
+    await page.click('[menu-name=tables]');
+    await page.click('[menu-name=simple]');
+    await page.waitForSelector('[my-table=simple] tbody tr [alt=INS]');
+    await page.click('[my-table=simple] tbody tr [alt=INS]');
+    await page.waitFor(500);
+    await page.type('[my-table=simple] tbody tr td', '333');
+    await page.screenshot({path: 'local-capture2.png'});
+    await page.keyboard.press('Enter');
+    await page.keyboard.type('tres tres tres');
+    await page.keyboard.press('Enter');
+    await page.waitFor(1000);
+    await browser.close();
+})();
