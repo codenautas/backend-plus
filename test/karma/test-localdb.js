cfde093642b33cd39fa88c559a50a72474141693
Emilio <emilioplatzer@gmail.com>
2018-06-19 12:17:53 -0300

    agregando pruebas en karma

---
+++ test/karma/test-localdb.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/// <reference path="../node_modules/types.d.ts/modules/myOwn/in-myOwn.d.ts" />
+/// <reference path="../../lib/in-backend-plus.d.ts" />
+/// <reference path="../../src/for-client/my-localdb.ts" />
+
+var LocalDb = require("my-localdb").LocalDb;
+
+describe("local-db", function(){
+    var ldb;
+    before(function(){
+        ldb = new LocalDb("the-test-name");
+    });
+    it("put and get structure", async function(){
+        var tableDef={
+            name:'this_name',
+            fields:[
+                {name: 'pk1', typeName:'text'}
+            ],
+            primaryKey:['pk1']
+        };
+        await ldb.registerStructure(tableDef);
+        var newTableDef = await ldb.getStructure(tableDef.name);
+        expect(tableDef).to.eql(newTableDef);
+    });
+});
