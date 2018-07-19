a763b0b3f91ca867f6a93ef37d5163d05a6cad50
Manuel De la Penna <manueldelapenna@gmail.com>
2018-07-19 14:55:05 -0300

    paso client side lock a backend plus

--- for-client/my-tables.js
+++ for-client/my-tables.js
@@ -2217,6 +2217,33 @@ myOwn.clientSides={
                 }
             }, true);
         }
+    },
+    $lock:{
+        update:true,
+        prepare:function(depot, fieldName){
+            depot.rowControls[fieldName].addEventListener('update',function(){
+                var control = this;
+                var valor = control.getTypedValue();
+                if((valor=='B' || valor=='⚿') && "no estaba lockeado"){
+                    control.setTypedValue('⌚');
+                    my.ajax.table["lock-record"]({
+                        table:depot.def.name,
+                        primaryKeyValues:depot.primaryKeyValues
+                    }).then(function(result){
+                        var tables=[depot.def.name].concat(depot.def.offline.details)
+                        var promiseChain=Promise.resolve();
+                        tables.forEach(function(name,i){
+                            promiseChain=promiseChain.then(function(){
+                                my.ldb.putMany(name,result.data[i]);
+                            })
+                        })
+                        return promiseChain;
+                    }).then(function(){
+                        control.setTypedValue('⚿');
+                    })
+                }
+            })
+        }
     }
 };
 
