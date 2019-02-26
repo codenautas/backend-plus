e705278ec4f3ae96ddaaea551c62d633d6ff1f25
Emilio <emilioplatzer@gmail.com>
2019-02-26 15:32:32 -0300

    arreglo el problema de la actualización violeta

--- examples/tables/server/table-new_element.js
+++ examples/tables/server/table-new_element.js
@@ -11,7 +11,7 @@ module.exports = function(context){
         elementName:'new element',
         forInsertOnlyMode:true,
         layout:{vertical:true},
-        // sql:{isTable:false}
+        sql:{isTable:false}
     });
     return defNewElement;
 }
