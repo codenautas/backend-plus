8570499fc510224aab452b66d371db4ac28d2911
Emilio Platzer <emilioplatzer@gmail.com>
2021-05-01 15:38:15 -0300

    Agrego .zip en las extensiones automáticas y jsona desde type-store

--- lib/backend-plus.d.ts
+++ lib/backend-plus.d.ts
@@ -146,7 +146,7 @@ export interface TableContext extends Context{
     superuser?:true
     forDump?:boolean
 }
-export type PgKnownTypes='decimal'|'text'|'boolean'|'integer'|'bigint'|'date'|'interval'|'timestamp'|'jsonb'|'double'|'bytea';
+export type PgKnownTypes='decimal'|'text'|'boolean'|'integer'|'bigint'|'date'|'interval'|'timestamp'|'jsonb'|'double'|'bytea'|'jsona';
 export type PgKnownDbValues='current_timestamp'|'current_user'|'session_user';
 export type SequenceDefinition = {
     name:string
