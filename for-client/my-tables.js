0c41a42da2e159a9dbc808fb50b3192137accc52
ebtonini <ebtonini@gmail.com>
2023-02-24 11:12:51 -0300

    con alias para varias fk sobre la misma tabla

--- for-client/my-tables.js
+++ for-client/my-tables.js
@@ -2083,7 +2083,7 @@ myOwn.TableGrid.prototype.prepareGrid = function prepareGrid(){
                         var foreignFieldName =  fieldDef.references == fkDef.references ? fieldDef.referencedName : fieldDef.inherited ? fieldDef.name : null;
                         if (referencedRow && foreignFieldName in referencedRow) {
                             var lookupValue = referencedRow[foreignFieldName];
-                            var fieldName = fieldDef.name;
+                            var fieldName = fkDef.alias + '__' + displayFieldName;
                             if(fieldDef.inherited && !sameValue(depot.row[fieldName], lookupValue)){
                                 depot.rowPendingForUpdate[fieldName] = lookupValue;
                             }
